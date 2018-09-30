36a0434a6d8a738629f6dba2a8118275945b93d6
maritomo <mari@champ.hep.sci.osaka-u.ac.jp>
2018-09-30 09:35:06 +0900

    set tab size to 4

--- include/CosmicTriggerSystem.h
+++ include/CosmicTriggerSystem.h
@@ -25,74 +25,74 @@
 
 class CosmicTriggerSystem {
 
-public:
-  CosmicTriggerSystem(int runID);
-  ~CosmicTriggerSystem();
+  public:
+    CosmicTriggerSystem(int runID);
+    ~CosmicTriggerSystem();
 
-  bool Init();
-  bool Init_map();
-  bool Init_channelDelay();
-  bool Init_calibConst();
-  bool Init_hitCondition();
+    bool Init();
+    bool Init_map();
+    bool Init_channelDelay();
+    bool Init_calibConst();
+    bool Init_hitCondition();
 
-  void Process();
-  void HitDecision();
-  void Tracking();
+    void Process();
+    void HitDecision();
+    void Tracking();
 
-  void SetData(int slot, int ch, const short* data);
+    void SetData(int slot, int ch, const short* data);
 
-  int GetNCRC() { return m_nCRC; }
-  int GetNLayer() { return m_nLayer; }
-  int GetNTrack() { return m_nTrack; }
+    int GetNCRC() { return m_nCRC; }
+    int GetNLayer() { return m_nLayer; }
+    int GetNTrack() { return m_nTrack; }
 
-  int GetIsInit() { return m_isInit; }
-  int GetRunID() { return m_runID; }
+    int GetIsInit() { return m_isInit; }
+    int GetRunID() { return m_runID; }
 
-  bool GetLocationID(int scintiID, int& layerID, int& ch);
-  CosmicRayCounter* GetCRC(int layer, int ch) { return m_crc[layer][ch]; }
+    bool GetLocationID(int scintiID, int& layerID, int& ch);
+    CosmicRayCounter* GetCRC(int layer, int ch) { return m_crc[layer][ch]; }
 
-  double GetCommonThreshold() { return m_comthr; }
-  double GetfPedestalSigma() { return m_fPedSig; }
+    double GetCommonThreshold() { return m_comthr; }
+    double GetfPedestalSigma() { return m_fPedSig; }
 
-  int GetNhit(int layer) { return m_nHit[layer]; }
-  int GetHit(int layer) { return m_hitCh[layer]; }
-  double GetHitPos(int layer, int axis) { return m_hitpos[layer][axis]; }
+    int GetNhit(int layer) { return m_nHit[layer]; }
+    int GetHit(int layer) { return m_hitCh[layer]; }
+    double GetHitPos(int layer, int axis) { return m_hitpos[layer][axis]; }
 
-  int GetTrackID() { return m_trackID; }
+    int GetTrackID() { return m_trackID; }
 
-  void GetVisAxis(int plane, int& axis_h, int& axis_v);
-  void Visualize();
-  void SetTrackLine(int plane);
-  void Display();
-  void Print(const char* filename);
+    void GetVisAxis(int plane, int& axis_h, int& axis_v);
+    void Visualize();
+    void SetTrackLine(int plane);
+    void Display();
+    void Print(const char* filename);
 
-private:
-  static const int m_nLayer = 2;  // # of layer (top, bottom)
-  static const int m_nCRC = 6;  // # of cosmic ray counter in 1 layer
-  static const int m_nTrack = 36;
+  private:
+    static const int m_nLayer = 2;  // # of layer (top, bottom)
+    static const int m_nCRC = 6;  // # of cosmic ray counter in 1 layer
+    static const int m_nTrack = 36;
 
-  int m_isInit;
-  int m_runID;
+    int m_isInit;
+    int m_runID;
 
-  CosmicRayCounter* m_crc[m_nLayer][m_nCRC];
+    CosmicRayCounter* m_crc[m_nLayer][m_nCRC];
 
-  double m_comthr; // common peak threshold
-  double m_fPedSig; // peak threshold = fPedSig * pedestalRMS
+    double m_comthr; // common peak threshold
+    double m_fPedSig; // peak threshold = fPedSig * pedestalRMS
 
-  int m_nHit[2];
-  int m_hitCh[2];
-  double m_hitpos[2][3];
+    int m_nHit[2];
+    int m_hitCh[2];
+    double m_hitpos[2][3];
 
-  int m_trackID;
-  double m_track[3][2]; // (xy, zy, xz) plane, (constant, slope)
-  double TOF; // time of flight
+    int m_trackID;
+    double m_track[3][2]; // (xy, zy, xz) plane, (constant, slope)
+    double TOF; // time of flight
 
-  int m_isVis[3];
-  double m_world[3][2]; // (x, y, z)
+    int m_isVis[3];
+    double m_world[3][2]; // (x, y, z)
 
-  TCanvas* m_canv;
-  TH1* m_frame[3];
-  TLine* m_lTrack[3];
+    TCanvas* m_canv;
+    TH1* m_frame[3];
+    TLine* m_lTrack[3];
 };
 
 #endif //CONV_COSMITRIGGER_H
--- src/CosmicTriggerSystem.cpp
+++ src/CosmicTriggerSystem.cpp
@@ -14,58 +14,58 @@
 
 CosmicTriggerSystem::CosmicTriggerSystem(int runID) : m_runID(runID) {
 
-  if(!Init()) {
-    std::cout << "Error: CosmicTriggerSystem was not initialized\n";
-  }
-
-  for(int plane = 0; plane < 3; ++plane) {
-    m_isVis[plane] = 0;
-  }
-
-  double worldsize[3] = {1100, 1500, 300};
-  for(int axis = 0; axis < 3; ++axis) {
-    for(int i = 0; i < 2; ++i) {
-      m_world[axis][i] = worldsize[axis];
-      if(i==0) m_world[axis][i] *= -1;
+    if(!Init()) {
+        std::cout << "Error: CosmicTriggerSystem was not initialized\n";
     }
-  }
+
+    for(int plane = 0; plane < 3; ++plane) {
+        m_isVis[plane] = 0;
+    }
+
+    double worldsize[3] = {1100, 1500, 300};
+    for(int axis = 0; axis < 3; ++axis) {
+        for(int i = 0; i < 2; ++i) {
+            m_world[axis][i] = worldsize[axis];
+            if(i==0) m_world[axis][i] *= -1;
+        }
+    }
 
 }
 
 CosmicTriggerSystem::~CosmicTriggerSystem() {
-  for(int layer = 0; layer < m_nLayer; ++layer) {
-    for(int ch = 0; ch < m_nCRC; ++ch) {
-      delete m_crc[layer][ch];
+    for(int layer = 0; layer < m_nLayer; ++layer) {
+        for(int ch = 0; ch < m_nCRC; ++ch) {
+            delete m_crc[layer][ch];
+        }
     }
-  }
 
-  if(m_isVis[0]==1 || m_isVis[1]==1 || m_isVis[2]==1) {
-    m_canv->Delete();
-    for(int plane = 0; plane < 3; ++plane) {
-      m_lTrack[plane]->Delete();
+    if(m_isVis[0]==1 || m_isVis[1]==1 || m_isVis[2]==1) {
+        m_canv->Delete();
+        for(int plane = 0; plane < 3; ++plane) {
+            m_lTrack[plane]->Delete();
+        }
     }
-  }
 }
 
 bool CosmicTriggerSystem::Init() {
 
-  m_isInit = 0;
+    m_isInit = 0;
 
-  std::cout << "---------------------------------------------\n";
-  std::cout << "      Initialize cosmic trigger system       \n";
-  std::cout << "---------------------------------------------\n";
+    std::cout << "---------------------------------------------\n";
+    std::cout << "      Initialize cosmic trigger system       \n";
+    std::cout << "---------------------------------------------\n";
 
-  if(!Init_map()) return false;
-  if(!Init_channelDelay()) return false;
-  if(!Init_calibConst()) return false;
-  if(!Init_hitCondition()) return false;
+    if(!Init_map()) return false;
+    if(!Init_channelDelay()) return false;
+    if(!Init_calibConst()) return false;
+    if(!Init_hitCondition()) return false;
 
-  std::cout << "---------------------------------------------\n";
-  std::cout << "      Cosmic trigger system initialized      \n";
-  std::cout << "---------------------------------------------\n";
+    std::cout << "---------------------------------------------\n";
+    std::cout << "      Cosmic trigger system initialized      \n";
+    std::cout << "---------------------------------------------\n";
 
-  m_isInit = 1;
-  return true;
+    m_isInit = 1;
+    return true;
 }
 
 /*
@@ -74,144 +74,144 @@ bool CosmicTriggerSystem::Init() {
 
 bool CosmicTriggerSystem::Init_map() {
 
-  std::string filename = "./data/map_crc.txt";
-  std::ifstream ifs(filename.c_str());
+    std::string filename = "./data/map_crc.txt";
+    std::ifstream ifs(filename.c_str());
 
-  if(!ifs) {
-    std::cout << filename << " not found\n";
-    return false;
-  }
-
-  int scintiID, dir;
-  double pos[3];
-
-  for(int layer = 0; layer < m_nLayer; ++layer) {
-    for(int ch = 0; ch < m_nCRC; ++ch) {
-      ifs >> scintiID >> dir >> pos[0] >> pos[1] >> pos[2];
-      m_crc[layer][ch] = new CosmicRayCounter(layer, ch, scintiID, dir, pos);
+    if(!ifs) {
+        std::cout << filename << " not found\n";
+        return false;
     }
-  }
 
-  ifs.close();
+    int scintiID, dir;
+    double pos[3];
 
-  std::cout << "Trigger counter map             [OK]\n";
-  return true;
+    for(int layer = 0; layer < m_nLayer; ++layer) {
+        for(int ch = 0; ch < m_nCRC; ++ch) {
+            ifs >> scintiID >> dir >> pos[0] >> pos[1] >> pos[2];
+            m_crc[layer][ch] = new CosmicRayCounter(layer, ch, scintiID, dir, pos);
+        }
+    }
+
+    ifs.close();
+
+    std::cout << "Trigger counter map             [OK]\n";
+    return true;
 }
 
 bool CosmicTriggerSystem::Init_channelDelay() {
 
-  std::string filename = "./data/channel_delay.txt";
-  std::ifstream ifs(filename.c_str());
+    std::string filename = "./data/channel_delay.txt";
+    std::ifstream ifs(filename.c_str());
 
-  if(!ifs) {
-    std::cout << filename << " not found\n";
-    return false;
-  }
+    if(!ifs) {
+        std::cout << filename << " not found\n";
+        return false;
+    }
 
-  int slot, adcch, delay;
+    int slot, adcch, delay;
 
-  while(ifs >> slot >> adcch >> delay) {
-    int layer = slot;
-    int ch = adcch / 2;
-    int side = adcch % 2;
-    m_crc[layer][ch]->SetDelay(side, delay);
-  }
+    while(ifs >> slot >> adcch >> delay) {
+        int layer = slot;
+        int ch = adcch / 2;
+        int side = adcch % 2;
+        m_crc[layer][ch]->SetDelay(side, delay);
+    }
 
-  ifs.close();
+    ifs.close();
 
-  std::cout << "Channel delays                  [OK]\n";
-  return true;
+    std::cout << "Channel delays                  [OK]\n";
+    return true;
 }
 
 bool CosmicTriggerSystem::Init_calibConst() {
 
-  std::string filename = "./data/TDtoX.txt";
-  std::ifstream ifs(filename.c_str());
+    std::string filename = "./data/TDtoX.txt";
+    std::ifstream ifs(filename.c_str());
 
-  if(!ifs) {
-    std::cout << filename << " not found\n";
-    return false;
-  }
-
-  for(int i = 0; i < 16; ++i) {
-    int scintiID;
-    double cc[2];
-    ifs >> scintiID >> cc[0] >> cc[1];
-
-    int layer, ch;
-    if(GetLocationID(scintiID, layer, ch)) {
-      m_crc[layer][ch]->SetCalibConst(cc);
+    if(!ifs) {
+        std::cout << filename << " not found\n";
+        return false;
     }
-  }
 
-  ifs.close();
+    for(int i = 0; i < 16; ++i) {
+        int scintiID;
+        double cc[2];
+        ifs >> scintiID >> cc[0] >> cc[1];
 
-  std::cout << "Calibration constants           [OK]\n";
-  return true;
+        int layer, ch;
+        if(GetLocationID(scintiID, layer, ch)) {
+            m_crc[layer][ch]->SetCalibConst(cc);
+        }
+    }
+
+    ifs.close();
+
+    std::cout << "Calibration constants           [OK]\n";
+    return true;
 }
 
 bool CosmicTriggerSystem::Init_hitCondition() {
 
-  m_fPedSig = 5; // peak threhold = pedestalRMS * fPedSig
+    m_fPedSig = 5; // peak threhold = pedestalRMS * fPedSig
 
-  std::stringstream ss;
-  ss << "./data/pedestal/pedestal_run" << m_runID << ".txt";
-  std::string filename = ss.str();
+    std::stringstream ss;
+    ss << "./data/pedestal/pedestal_run" << m_runID << ".txt";
+    std::string filename = ss.str();
 
 #ifdef COMMON_THRESHOLD
-  filename = "./data/common_threshold.txt";
+    filename = "./data/common_threshold.txt";
 #endif
 
-  std::ifstream ifs(filename.c_str());
-  if(!ifs) {
-    std::cout << filename << " not found\n";
-    return false;
-  }
+    std::ifstream ifs(filename.c_str());
+    if(!ifs) {
+        std::cout << filename << " not found\n";
+        return false;
+    }
 
-  int slot, adcch;
-  int layer, ch, side;
-  double ped_mean, ped_sigma;
+    int slot, adcch;
+    int layer, ch, side;
+    double ped_mean, ped_sigma;
 
-  while(ifs >> slot >> adcch >> ped_mean >> ped_sigma) {
-    layer = slot;
-    ch = adcch / 2;
-    side = adcch % 2;
-    
+    while(ifs >> slot >> adcch >> ped_mean >> ped_sigma) {
+        layer = slot;
+        ch = adcch / 2;
+        side = adcch % 2;
+
 #ifdef COMMON_THRESHOLD
-    m_comthr = ped_sigma;
+        m_comthr = ped_sigma;
 #else
-    ped_sigma *= nSigma;
+        ped_sigma *= nSigma;
 #endif
 
-    m_crc[layer][ch]->SetPeakThreshold(side, ped_sigma);
-  }
+        m_crc[layer][ch]->SetPeakThreshold(side, ped_sigma);
+    }
 
-  ifs.close();
-
-
-  filename = "./data/coincidence_range.txt";
-  std::ifstream ifs2(filename.c_str());
-  if(!ifs2) {
-    std::cout << filename << " not found\n";
-    return false;
-  }
-
-  double coin_range[2];
-
-  while(ifs2 >> slot >> adcch >> coin_range[0] >> coin_range[1]) {
-    int layer = slot;
-    int ch = adcch / 2;
-    int side = adcch % 2;
-
-    m_crc[layer][ch]->SetCoinRange_min(side, coin_range[0]);
-    m_crc[layer][ch]->SetCoinRange_max(side, coin_range[1]);
-  }
-
-  ifs2.close();
+    ifs.close();
 
 
-  std::cout << "Hit condition parameters        [OK]\n";
-  return true;
+    filename = "./data/coincidence_range.txt";
+    std::ifstream ifs2(filename.c_str());
+    if(!ifs2) {
+        std::cout << filename << " not found\n";
+        return false;
+    }
+
+    double coin_range[2];
+
+    while(ifs2 >> slot >> adcch >> coin_range[0] >> coin_range[1]) {
+        int layer = slot;
+        int ch = adcch / 2;
+        int side = adcch % 2;
+
+        m_crc[layer][ch]->SetCoinRange_min(side, coin_range[0]);
+        m_crc[layer][ch]->SetCoinRange_max(side, coin_range[1]);
+    }
+
+    ifs2.close();
+
+
+    std::cout << "Hit condition parameters        [OK]\n";
+    return true;
 }
 
 /*
@@ -219,54 +219,54 @@ bool CosmicTriggerSystem::Init_hitCondition() {
  */
 
 void CosmicTriggerSystem::Process() {
-  HitDecision();
-  Tracking();
+    HitDecision();
+    Tracking();
 }
 
 void CosmicTriggerSystem::HitDecision() {
-  m_nHit[0] = 0;
-  m_nHit[1] = 0;
+    m_nHit[0] = 0;
+    m_nHit[1] = 0;
 
-  for(int layer = 0; layer < m_nLayer; ++layer) {
-    for(int ch = 0; ch < m_nCRC; ++ch) {
-      m_crc[layer][ch]->Process();
+    for(int layer = 0; layer < m_nLayer; ++layer) {
+        for(int ch = 0; ch < m_nCRC; ++ch) {
+            m_crc[layer][ch]->Process();
 
-      if(m_crc[layer][ch]->GetIsHit()) {
-        ++m_nHit[layer];
-        m_hitCh[layer] = ch;
-      }
+            if(m_crc[layer][ch]->GetIsHit()) {
+                ++m_nHit[layer];
+                m_hitCh[layer] = ch;
+            }
+        }
     }
-  }
 
 }
 
 void CosmicTriggerSystem::Tracking() {
 
-  if(m_nHit[0]!=1 || m_nHit[1]!=1) {
-    m_trackID = 0;
-    return;
-  }
-
-  for(int layer = 0; layer < m_nLayer; ++layer) {
-    for(int axis = 0; axis < 3; ++axis) {
-      m_hitpos[layer][axis] = m_crc[layer][m_hitCh[layer]]->GetHitPos(axis);
+    if(m_nHit[0]!=1 || m_nHit[1]!=1) {
+        m_trackID = 0;
+        return;
     }
-  }
 
-  m_trackID = m_hitCh[1] * m_nCRC + m_hitCh[0] + 1;
+    for(int layer = 0; layer < m_nLayer; ++layer) {
+        for(int axis = 0; axis < 3; ++axis) {
+            m_hitpos[layer][axis] = m_crc[layer][m_hitCh[layer]]->GetHitPos(axis);
+        }
+    }
 
-  TOF = sqrt((m_hitpos[1][0] - m_hitpos[0][0]) * (m_hitpos[1][0] - m_hitpos[0][0]) +
-             (m_hitpos[1][1] - m_hitpos[0][1]) * (m_hitpos[1][1] - m_hitpos[0][1]) +
-             (m_hitpos[1][2] - m_hitpos[0][2]) * (m_hitpos[1][2] - m_hitpos[0][2])) / TMath::C();
+    m_trackID = m_hitCh[1] * m_nCRC + m_hitCh[0] + 1;
 
-  int axis_h; // index of horizontal axis
-  int axis_v; // index of vertical axis
+    TOF = sqrt((m_hitpos[1][0] - m_hitpos[0][0]) * (m_hitpos[1][0] - m_hitpos[0][0]) +
+               (m_hitpos[1][1] - m_hitpos[0][1]) * (m_hitpos[1][1] - m_hitpos[0][1]) +
+               (m_hitpos[1][2] - m_hitpos[0][2]) * (m_hitpos[1][2] - m_hitpos[0][2])) / TMath::C();
 
-  for(int plane = 0; plane < 3; ++plane) {
-    GetVisAxis(plane, axis_h, axis_v);
-    m_track[plane][1] = (m_hitpos[1][axis_v] - m_hitpos[0][axis_v]) / (m_hitpos[1][axis_h] - m_hitpos[0][axis_h]);
-    m_track[plane][0] = m_hitpos[1][axis_v] - m_track[plane][1] * m_hitpos[1][axis_h];
-  }
+    int axis_h; // index of horizontal axis
+    int axis_v; // index of vertical axis
+
+    for(int plane = 0; plane < 3; ++plane) {
+        GetVisAxis(plane, axis_h, axis_v);
+        m_track[plane][1] = (m_hitpos[1][axis_v] - m_hitpos[0][axis_v]) / (m_hitpos[1][axis_h] - m_hitpos[0][axis_h]);
+        m_track[plane][0] = m_hitpos[1][axis_v] - m_track[plane][1] * m_hitpos[1][axis_h];
+    }
 
 }
 
@@ -275,14 +275,14 @@ void CosmicTriggerSystem::Tracking() {
  */
 
 void CosmicTriggerSystem::SetData(int slot, int ch, const short* data) {
-  if(slot > 1) return;
-  if(ch > 11) return;
+    if(slot > 1) return;
+    if(ch > 11) return;
 
-  int layer = slot;
-  int channel = ch / 2;
-  int LR = ch % 2;
+    int layer = slot;
+    int channel = ch / 2;
+    int LR = ch % 2;
 
-  m_crc[layer][channel]->SetData(LR, data);
+    m_crc[layer][channel]->SetData(LR, data);
 }
 
 /*
@@ -290,18 +290,18 @@ void CosmicTriggerSystem::SetData(int slot, int ch, const short* data) {
  */
 
 bool CosmicTriggerSystem::GetLocationID(int scintiID, int& layerID, int& chID) {
-  for(int layer = 0; layer < m_nLayer; ++layer) {
-    for(int ch = 0; ch < m_nCRC; ++ch) {
-      if(m_crc[layer][ch]->GetScintiID()==scintiID) {
-        layerID = layer;
-        chID = ch;
-        return true;
-      }
+    for(int layer = 0; layer < m_nLayer; ++layer) {
+        for(int ch = 0; ch < m_nCRC; ++ch) {
+            if(m_crc[layer][ch]->GetScintiID()==scintiID) {
+                layerID = layer;
+                chID = ch;
+                return true;
+            }
+        }
     }
-  }
 
-  std::cout << "(scinti. " << scintiID << " not found)\n";
-  return false;
+    std::cout << "(scinti. " << scintiID << " not found)\n";
+    return false;
 }
 
 /*
@@ -309,112 +309,112 @@ bool CosmicTriggerSystem::GetLocationID(int scintiID, int& layerID, int& chID) {
  */
 
 void CosmicTriggerSystem::GetVisAxis(int plane, int& axis_h, int& axis_v) {
-  if(plane==0) {          // xy plane -> (x, y)
-    axis_h = 0;
-    axis_v = 1;
-  } else if(plane==1) {   // yz plane -> (z, y)
-    axis_h = 2;
-    axis_v = 1;
-  } else if(plane==2) {   // zx plane -> (x, z)
-    axis_h = 0;
-    axis_v = 2;
-  } else {
-    std::cout << "plane: 0 (xy), 1 (yz), 2 (zx)\n";
-  }
+    if(plane==0) {          // xy plane -> (x, y)
+        axis_h = 0;
+        axis_v = 1;
+    } else if(plane==1) {   // yz plane -> (z, y)
+        axis_h = 2;
+        axis_v = 1;
+    } else if(plane==2) {   // zx plane -> (x, z)
+        axis_h = 0;
+        axis_v = 2;
+    } else {
+        std::cout << "plane: 0 (xy), 1 (yz), 2 (zx)\n";
+    }
 }
 
 void CosmicTriggerSystem::Visualize() {
-  m_canv = new TCanvas("m_canv", "event display", 900, 600);
-  m_canv->Divide(2, 1);
+    m_canv = new TCanvas("m_canv", "event display", 900, 600);
+    m_canv->Divide(2, 1);
 
-  // xy and yz plane
-  for(int plane = 0; plane < 2; ++plane) {
-    for(int layer = 0; layer < m_nLayer; ++layer) {
-      for(int ch = 0; ch < m_nCRC; ++ch) {
-        m_crc[layer][ch]->Visualize(plane);
-      }
+    // xy and yz plane
+    for(int plane = 0; plane < 2; ++plane) {
+        for(int layer = 0; layer < m_nLayer; ++layer) {
+            for(int ch = 0; ch < m_nCRC; ++ch) {
+                m_crc[layer][ch]->Visualize(plane);
+            }
+        }
+
+        m_lTrack[plane] = new TLine();
+        m_lTrack[plane]->SetLineColor(kRed);
+
+        m_isVis[plane] = 1;
     }
-
-    m_lTrack[plane] = new TLine();
-    m_lTrack[plane]->SetLineColor(kRed);
-
-    m_isVis[plane] = 1;
-  }
 
 }
 
 void CosmicTriggerSystem::SetTrackLine(int plane) {
 
-  int axis_h; // index of horizontal axis
-  int axis_v; // index of vertical axis
-  GetVisAxis(plane, axis_h, axis_v);
+    int axis_h; // index of horizontal axis
+    int axis_v; // index of vertical axis
+    GetVisAxis(plane, axis_h, axis_v);
 
-  double h[2], v[2];
+    double h[2], v[2];
 
-  for(int i = 0; i < 2; ++i) {
-    h[i] = (m_world[axis_v][i] - m_track[plane][0]) / m_track[plane][1];
+    for(int i = 0; i < 2; ++i) {
+        h[i] = (m_world[axis_v][i] - m_track[plane][0]) / m_track[plane][1];
 
-    if(h[i] < m_world[axis_h][0]) {
-      h[i] = m_world[axis_h][0];
+        if(h[i] < m_world[axis_h][0]) {
+            h[i] = m_world[axis_h][0];
+        }
+        if(h[i] > m_world[axis_h][1]) {
+            h[i] = m_world[axis_h][1];
+        }
+
+
+        v[i] = m_track[plane][1] * h[i] + m_track[plane][0];
     }
-    if(h[i] > m_world[axis_h][1]) {
-      h[i] = m_world[axis_h][1];
-    }
 
-
-    v[i] = m_track[plane][1] * h[i] + m_track[plane][0];
-  }
-
-  m_lTrack[plane]->SetX1(h[0]);
-  m_lTrack[plane]->SetX2(h[1]);
-  m_lTrack[plane]->SetY1(v[0]);
-  m_lTrack[plane]->SetY2(v[1]);
+    m_lTrack[plane]->SetX1(h[0]);
+    m_lTrack[plane]->SetX2(h[1]);
+    m_lTrack[plane]->SetY1(v[0]);
+    m_lTrack[plane]->SetY2(v[1]);
 }
 
 void CosmicTriggerSystem::Display() {
-  if(m_isVis[0]==0 && m_isVis[1]==0 && m_isVis[2]==0) {
-    std::cout << "Visualization not ready\n";
-    return;
-  }
-
-  int axis_h; // index of horizontal axis
-  int axis_v; // index of vertical axis
-
-  TString title[3] = {";x [mm];y [mm]", ";z [mm];y [mm]", ";x [mm];z [mm]"};
-
-  // xy and yz plane
-  for(int plane = 0; plane < 2; ++plane) {
-    if(!m_isVis[plane]) continue;
-
-    GetVisAxis(plane, axis_h, axis_v);
-
-    m_canv->cd(plane + 1)->Clear();
-    m_canv->cd(plane + 1)->DrawFrame(m_world[axis_h][0], m_world[axis_v][0],
-                                     m_world[axis_h][1], m_world[axis_v][1], title[plane]);
-
-    // Cosmic ray counters
-    for(int layer = 0; layer < m_nLayer; ++layer) {
-      for(int ch = 0; ch < m_nCRC; ++ch) {
-        m_crc[layer][ch]->Display(plane);
-      }
+    if(m_isVis[0]==0 && m_isVis[1]==0 && m_isVis[2]==0) {
+        std::cout << "Visualization not ready\n";
+        return;
     }
 
-    m_canv->Update();
-    m_canv->Modified();
+    int axis_h; // index of horizontal axis
+    int axis_v; // index of vertical axis
 
-    // Track
-    if(m_trackID==0) continue;
+    TString title[3] = {";x [mm];y [mm]", ";z [mm];y [mm]", ";x [mm];z [mm]"};
 
-    SetTrackLine(plane);
-    m_lTrack[plane]->Draw("same");
+    // xy and yz plane
+    for(int plane = 0; plane < 2; ++plane) {
+        if(!m_isVis[plane]) continue;
 
-    m_canv->Update();
-    m_canv->Modified();
-  }
+        GetVisAxis(plane, axis_h, axis_v);
+
+        m_canv->cd(plane + 1)->Clear();
+        m_canv->cd(plane + 1)->DrawFrame(m_world[axis_h][0], m_world[axis_v][0],
+                                         m_world[axis_h][1], m_world[axis_v][1], title[plane]);
+
+        // Cosmic ray counters
+        for(int layer = 0; layer < m_nLayer; ++layer) {
+            for(int ch = 0; ch < m_nCRC; ++ch) {
+                m_crc[layer][ch]->Display(plane);
+            }
+        }
+
+        m_canv->Update();
+        m_canv->Modified();
+
+        // Track
+        if(m_trackID==0) continue;
+
+        SetTrackLine(plane);
+        m_lTrack[plane]->Draw("same");
+
+        m_canv->Update();
+        m_canv->Modified();
+    }
 
 }
 
 void CosmicTriggerSystem::Print(const char* filename) {
-  if(m_isVis[0]==1 || m_isVis[1]==1 || m_isVis[2]==1)
-    m_canv->Print(filename);
+    if(m_isVis[0]==1 || m_isVis[1]==1 || m_isVis[2]==1)
+        m_canv->Print(filename);
 }
