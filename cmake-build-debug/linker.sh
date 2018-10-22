0d10dcd7db4a4846ac54a6e16f7bf6c8b9441f99
maritomo <mari@champ.hep.sci.osaka-u.ac.jp>
2018-10-22 13:05:53 +0900

    pedestal data deleted

--- cmake-build-debug/linker.sh
+++ cmake-build-debug/linker.sh
@@ -2,9 +2,11 @@ unlink rootfile
 unlink convfile
 
 if [ $1 = "0" ]; then
-    echo "rootfile and convfile unlinked\n";
+    echo "unlink rootfile"
+    echo "unlink convfile"
 else
     ln -s ../../rootfile ./rootfile
     ln -s ../../rootfile ./convfile
-    echo "rootfile and convfile linked\n";
+    echo "ln -s ../../rootfile ./rootfile"
+    echo "ln -s ../../rootfile ./convfile"
 fi
